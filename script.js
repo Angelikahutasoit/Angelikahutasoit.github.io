2fd2e4f188d016217637b058a2baf4d984be1cef
Angelikahutasoit <angelikaputri934@gmail.com>
2025-03-12 14:52:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("Website Loaded!");
 });
+let position = 0;
+function moveBox() {
+    position += 5;
+    document.getElementById("box").style.left = position + "px";
+    if (position < window.innerWidth - 50) {
+        requestAnimationFrame(moveBox);
+    }
+}
+moveBox();
